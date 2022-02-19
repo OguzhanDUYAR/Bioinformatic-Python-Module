e5e85490cc56aff873705914f00c81dfcd8f3593
Oğuzhan DUYAR <oguzhan.duyar.ogresyus@gmail.com>
2022-02-20 01:11:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from odbiotools import fastatools as ft
+from odbiotools import gento as gt
+
+
+
+mygen=ft.fastostr("FASTA/sequence.fasta")
+
+print(mygen)
+
+mygentag=ft.fastotag("FASTA/sequence.fasta")
+
+print(mygentag)
+
+mycodon=gt.gentocod(mygen)
+
+print(mycodon)
